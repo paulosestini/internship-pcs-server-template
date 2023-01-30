29185ea5c43ea1367ebefdbc3325821267a2a546
paulosestini <24728744+paulosestini@users.noreply.github.com>
2023-01-30 01:13:40 -0300

    chore: adds container for test db and integrates it

--- src/test/java/com/poli/internship/UserTest.java
+++ src/test/java/com/poli/internship/UserTest.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootTest
 @AutoConfigureHttpGraphQlTester
+@ActiveProfiles("test")
 public class UserTest {
     @Autowired
     private HttpGraphQlTester tester;
